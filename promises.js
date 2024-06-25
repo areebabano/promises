0f2df52cccc2d5628e10fb67605d3e45fe4df05f
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-06-25 23:23:39 +0500

    promises in typescript

---
+++ promises.js
@@ -0,0 +1,32 @@
+"use strict";
+// Tea
+// Add water
+// After 3 second Add sugar
+// After 4 second Add Tea Leaves
+// After 5 second Add Milk
+// after 3 second Tea is Ready
+// promises .then().catch()
+function makeChai(ingredients, delay) {
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(ingredients);
+            //   resolve("successfully...");
+            reject("something went wrong!");
+        }, delay);
+    });
+    return promise;
+}
+// promise chaining
+console.log("Add Water...");
+makeChai("Add Sugar...", 3000)
+    .then(() => {
+    makeChai("Add Tea Leaves...", 4000)
+        .then(() => {
+        makeChai("Add Milk...", 5000)
+            .then(() => {
+            makeChai("Tea is Ready...", 3000)
+                .then((res) => console.log(res))
+                .catch((err) => console.log(err));
+        });
+    });
+});
